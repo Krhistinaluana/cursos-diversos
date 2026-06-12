628b160e3c367d70cb7d3a28f5f29952eb2ca02e
Luana Krhistina <KRHISTINA.TECH@GMAIL.COM>
2026-06-12 10:34:00 -0300

    Finalizando-exercicios
    Finalização da lista de exercicíos mod I

--- Python-curso-em-video/modulo-01/exercicios-python/exercício 33 .py
+++ Python-curso-em-video/modulo-01/exercicios-python/exercício 33 .py
@@ -1,1 +1,23 @@
 #Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
+n1 = int(input('Digite o 3 números inteiros:'))
+n2 = int(input(' '))
+n3 = int(input(' '))
+
+nMaior = n1
+nMenor = n1
+
+if (n2 > nMaior):
+    nMaior = n2
+
+if (n3 > nMaior):
+    nMaior = n3
+
+if (n2 < nMenor):
+    nMenor = n2
+
+if (n3 < nMenor):
+    nMenor = n3
+    
+
+print(f'O número maior entre os 3 é: {nMaior} e o menor:{nMenor}')
--- Python-curso-em-video/modulo-01/exercicios-python/exercício 35 .py
+++ Python-curso-em-video/modulo-01/exercicios-python/exercício 35 .py
@@ -1,1 +1,10 @@
 #Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se eles podem ou não formar um triângulo.
+
+reta1 = int(input('Digite o comprimento de três retas:'))
+reta2 = int(input(''))
+reta3 = int(input(''))
+
+if (reta1+reta2) > reta3 and (reta1+reta3) > reta2 and (reta2+reta3) > reta1:
+    print('É possível formar um triângulo')
+else:
+    print('Não é possível formar um triângulo.')

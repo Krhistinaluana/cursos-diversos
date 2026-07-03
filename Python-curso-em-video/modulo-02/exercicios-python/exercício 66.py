ee81c951d7c6328d7d5dd9dcb3c6e5da40ecb629
Luana Krhistina <KRHISTINA.TECH@GMAIL.COM>
2026-07-03 10:19:57 -0300

    elaboracao dos exercicios 66-71

---
+++ Python-curso-em-video/modulo-02/exercicios-python/exercício 66.py
@@ -0,0 +1,11 @@
+#Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (Desconsiderando o Flag)
+
+s = 0
+contador = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s = s +n
+    contador = contador + 1
+print(f'A soma entre os números foi: {s} e o total de números digitados foram {contador}')
---
+++ Python-curso-em-video/modulo-02/exercicios-python/exercício 68 .py
@@ -0,0 +1,27 @@
+#Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
+
+import random
+print('='*20)
+print(' Jogo PAR ou ÍMPAR')
+print('='*20)
+
+vitoriaJogador = 0
+vitoriaPc = 0
+
+while True:
+    escolha = str(input('Par ou Ímpar? ')) .strip() .upper() [0]
+    n = int(input('Digite sua jogada? '))
+
+    numeroPc = random.randint(1,10)
+    print(f'Meu número é {numeroPc}')
+
+    soma = n + numeroPc
+    if soma % 2 == 0 and escolha == 'P':
+        vitoriaJogador = vitoriaJogador + 1
+    elif soma % 2 ==1 and escolha == 'Í':   
+        vitoriaJogador = vitoriaJogador +1
+    else:
+        vitoriaPc = vitoriaPc + 1
+        break
+
+print(f'O jogador computou o total de {vitoriaJogador} vitórias')

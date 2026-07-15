76cd69b40dabfd53a7498d76dcf5e2fc6c62b287
Luana Krhistina <KRHISTINA.TECH@GMAIL.COM>
2026-07-15 00:32:55 -0300

    correcao exer 73-75

--- Python-curso-em-video/modulo-03/exercicios-python/exercício 75 .py
+++ Python-curso-em-video/modulo-03/exercicios-python/exercício 75 .py
@@ -2,8 +2,12 @@
 
 tuplaValores = (int(input('Digite o 1ºvalor: ',)), int(input('Digite o 2ºvalor: ')), int(input('Digite o 3ºvalor: ')), int(input('Digite o 4ºvalor: ')))
 
+print(f'Você digitou os valores {tuplaValores}')
 print(f'O número 9 apareceu {tuplaValores.count(9)}X')
-print(f'O valor 3 foi digitado na posição {tuplaValores.index(3)}')
+if 3 in tuplaValores:
+    print(f'O valor 3 foi digitado na posição {tuplaValores.index(3)+1}')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição')
 
 
 print('Os numeros pares são: ')

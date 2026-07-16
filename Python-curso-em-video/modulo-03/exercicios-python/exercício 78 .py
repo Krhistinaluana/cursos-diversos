72f31c3456b57b8f433b602dd0f1f1dbbb82a2bc
Luana Krhistina <KRHISTINA.TECH@GMAIL.COM>
2026-07-15 22:19:42 -0300

    exercicios 78 a 83

---
+++ Python-curso-em-video/modulo-03/exercicios-python/exercício 78 .py
@@ -0,0 +1,10 @@
+  #Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
+
+
+guardarLista = []
+for i in range (0,5):
+  n = int(input('Digite um valor: '))
+  guardarLista.append(n)
+
+print(f'O maior valor é {max(guardarLista)}')
+print(f'O menor valor é {min(guardarLista)}')clear

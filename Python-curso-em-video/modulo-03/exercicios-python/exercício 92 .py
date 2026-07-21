a4292191dae96ce406f602edc609dfa6c86e1f61
Luana Krhistina <KRHISTINA.TECH@GMAIL.COM>
2026-07-21 13:51:34 -0300

    Enunciado dos exercícios 90 - 95.

---
+++ Python-curso-em-video/modulo-03/exercicios-python/exercício 92 .py
@@ -0,0 +1,1 @@
+#Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário se por acaso a CTPS for diferente de zero, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, alpém da idade, com quanto sanos a pessoa vai se aposentar.(considerar 35 anos de contribuição)
